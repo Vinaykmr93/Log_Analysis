0662dc24ce9c4ba5f9a4b76d84129e76e975c0b1
Vinaykmr93 <34295140+Vinaykmr93@users.noreply.github.com>
2018-02-11 16:04:36 +0530

    edit indentation

--- LogAnalysis.py
+++ LogAnalysis.py
@@ -19,8 +19,8 @@ def main():
 
     c.execute(popular_articles)
     print("\n")
-    print ("Most popular article of all time:\n")
-    for (article_title, total_reads) in c.fetchall():
+    print("Most popular article of all time:\n")
+    for(article_title, total_reads) in c.fetchall():
         print("    {} - {} views".format(article_title, total_reads))
     print("\n")
     print("*"*70)
@@ -34,7 +34,7 @@ def main():
     group by article_id, author_name order by author_reads desc;
     """
     c.execute(popular_authors)
-    print ("Most popular authors:\n")
+    print("Most popular authors:\n")
     for(article_id, author_name, author_reads) in c.fetchall():
         print("    {} - {} - {} views".format(article_id, author_name,
               author_reads))
@@ -47,13 +47,14 @@ def main():
     """
 
     c.execute(error_calculation)
-    print ("Days with more than 1% error:\n")
+    print("Days with more than 1% error:\n")
     for(date, error_percentage) in c.fetchall():
         print("  {} - {} views\n".format(date, error_percentage))
     print("*"*70)
 
     c.close()  # Close cursor
     db.close()  # Close Database
+
 
 if __name__ == "__main__":
     main()
